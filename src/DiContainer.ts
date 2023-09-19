ed4118eeec6a474cf4afe7a65404057fdaf8086e
fraybabak <keanbabak@gmail.com>
2023-09-19 15:06:51 +0300

    feat: add post section to Di container

--- src/DiContainer.ts
+++ src/DiContainer.ts
@@ -7,6 +7,9 @@ import { CreateUserUseCase } from './application/User/usecases/createUser';
 import { AuthenticateUserUseCase } from './application/User/usecases/authenticateUser';
 import { UserRepository } from './adapter/secondary/user/UserRepository';
 import { UserEventhandler } from './adapter/secondary/user/UserEventHandler';
+import { CreatePostUseCase } from './application/Post/usecases/createPost';
+import { PostRepository } from './adapter/secondary/post/PostRepository';
+import { PostEventhandler } from './adapter/secondary/post/PostEventHandler';
 
 container.registerSingleton('UserCreateController', UserCreateController)
 container.registerSingleton('FindUserUseCase', FindUserUseCase)
@@ -15,6 +18,11 @@ container.registerSingleton('UserRepository', UserRepository)
 container.registerSingleton('AuthUserController', AuthUserController)
 container.registerSingleton('AuthenticateUserUseCase', AuthenticateUserUseCase)
 container.registerSingleton('UserEventHandler', UserEventhandler)
+
+
+container.registerSingleton('CreatePostUseCase', CreatePostUseCase)
+container.registerSingleton('PostRepository', PostRepository)
+container.registerSingleton('PostEventHandler', PostEventhandler)
 
 export const userCreateController = container.resolve(UserCreateController)
 export const authUserController = container.resolve(AuthUserController)
